c2fd577547f8d8044f28cf105f1927e55d05b889
Dmytro Korlotian <dima@korlotian.pl>
2016-07-25 14:13:57 +0200

    connect user with pint

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725103240) do
+ActiveRecord::Schema.define(version: 20160725121146) do
 
   create_table "pints", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pints_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
