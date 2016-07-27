eb0d14d19e9369a6aa7569c521bc6787b80b7669
Dmytro Korlotian <dima@korlotian.pl>
2016-07-27 19:13:47 +0200

    move credentials to initializer

---
+++ config/initializers/paperclip_defaults.rb
@@ -0,0 +1,9 @@
+Paperclip::Attachment.default_options.merge!(
+    storage: :s3,
+    s3_region: ENV['AWS_REGION'],
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+)
